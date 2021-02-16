dfc34903080d37b9475509a19677a2bbdbd29ade
jehdz <jhernandez23@luc.edu>
2021-02-15 18:02:47 -0600

    ready for deployment

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -27,7 +27,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://intense-woodland-42293.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
